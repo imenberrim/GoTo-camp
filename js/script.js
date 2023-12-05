1bd83428964c708292e69ea3bdd1c347101a3f3b
imenberim <benrimimen12@gmail.com>
2023-12-05 19:53:42 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const menubtn =document.querySelector('#menu-btn');
 const navbar = document.querySelector('.header .navbar')
 
-menubtn.addEventListener('click', ()=>{
+menubtn.addEventListener('click',()=>{
     navbar.classList.toggle('active');
     menubtn.classList.toggle('fa-times');
-}
-)
+})
+
+
+const swiper = new Swiper('.home-slider', {
+   
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
