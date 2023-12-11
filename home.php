3a585c01b30fc8d4c1798b054308f500f93cd07f
imenberim <benrimimen12@gmail.com>
2023-12-12 00:38:51 +0100

    img

--- home.php
+++ home.php
@@ -6,13 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 
-   <!-- swiper css link-->
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     
-<!--font awesome cdn link-->
 <link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css>
 
-<!--custom css file link-->
  <link rel="stylesheet" href="css/style.css">  
 
   </head>
@@ -183,10 +180,9 @@
 <!--footer section ends-->
 
 
-  <!-- swiper js link-->
+
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-<!--custom js file link -->
 <script src="js/script.js"></script>
 
   </body>
